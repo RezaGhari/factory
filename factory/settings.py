09b5623badec8cf757320d39bb77e7f171410466
92reza <reza.ghari1995@gmail.com>
2020-03-13 19:05:54 +0330

    Adding Template folder: base.html and index.html

--- factory/settings.py
+++ factory/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'factory.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,4 +123,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+STATIC_FILES_DIRS = [
+    os.path.join(BASE_DIR, 'factory/static')
+]
